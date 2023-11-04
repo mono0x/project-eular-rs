43b74bf5db8e92e75706c6a026b18b3f60f9aee6
mono <mono@mono0x.net>
2023-11-04 23:23:55 +0900

    Add problem_0005

---
+++ problem_0005/src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+    println!("{}", problem_0005());
+}
+
+// https://projecteuler.net/problem=5
+fn problem_0005() -> u32 {
+    for i in (20..).step_by(20) {
+        if (1..=20).all(|x| i % x == 0) {
+            return i;
+        }
+    }
+    panic!("No solution found");
+}
+
+#[test]
+fn test_problem_0005() {
+    assert_eq!(problem_0005(), 232792560);
+}
