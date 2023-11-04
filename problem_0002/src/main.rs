e6c09714b372000c436be9304f72035a423b4841
mono <mono@mono0x.net>
2023-11-04 21:48:40 +0900

    Add problem_0002

---
+++ problem_0002/src/main.rs
@@ -0,0 +1,24 @@
+fn main() {
+    println!("{}", problem_0002());
+}
+
+// https://projecteuler.net/problem=2
+fn problem_0002() -> u32 {
+    let mut a: u32 = 1;
+    let mut b: u32 = 2;
+    let mut sum: u32 = b;
+    while b < 4_000_000 {
+        let next = a + b;
+        if next % 2 == 0 {
+            sum += next
+        }
+        a = b;
+        b = next
+    }
+    sum
+}
+
+#[test]
+fn test_problem_0002() {
+    assert_eq!(problem_0002(), 4613732);
+}
