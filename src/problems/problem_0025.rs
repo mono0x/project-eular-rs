c8f95ab8fe51c7f860478368640775fe762c1003
mono <mono@mono0x.net>
2023-11-18 05:05:39 +0000

    Refactor problem_0025

--- src/problems/problem_0025.rs
+++ src/problems/problem_0025.rs
@@ -1,40 +1,20 @@
-use std::collections::HashMap;
-
 use num_bigint::BigUint;
 
 // https://projecteuler.net/problem=25
 #[allow(dead_code)]
 fn problem_0025() -> u64 {
-    struct State {
-        memo: HashMap<u64, BigUint>,
-    }
-
-    impl State {
-        fn fib(&mut self, n: u64) -> BigUint {
-            if let Some(f) = self.memo.get(&n) {
-                return f.clone();
-            }
-            let f = if n <= 2 {
-                BigUint::from(1u64)
-            } else {
-                self.fib(n - 1) + self.fib(n - 2)
-            };
-            self.memo.insert(n, f.clone());
-            f
-        }
-    }
-
-    let mut state = State {
-        memo: HashMap::new(),
-    };
+    let mut a = BigUint::from(1u64);
+    let mut b = BigUint::from(1u64);
 
     let limit = BigUint::from(10u64).pow(999);
 
-    for i in 1.. {
-        let f = state.fib(i);
-        if f >= limit {
+    for i in 3.. {
+        let c = &a + &b;
+        if c >= limit {
             return i;
         }
+        a = b;
+        b = c;
     }
     panic!("must not happen")
 }
