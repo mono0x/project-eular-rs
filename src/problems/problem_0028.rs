9af07b93937fefaef30db4a27d3aa7cf7bb4259e
mono <mono@mono0x.net>
2023-11-19 14:34:52 +0900

    Refactor problem_0028

--- src/problems/problem_0028.rs
+++ src/problems/problem_0028.rs
@@ -1,83 +1,14 @@
 // https://projecteuler.net/problem=28
 #[allow(dead_code)]
-fn problem_0028(n: usize) -> u64 {
-    let mut table = vec![vec![0u64; n]; n];
-
-    let mut c = 1;
-    let mut l = 0;
-    let mut x = n / 2;
-    let mut y = n / 2;
-    loop {
-        l += 1;
-
-        let mut done = false;
-
-        // right
-        for _ in 0..l {
-            table[y][x] = c;
-            if x >= n - 1 {
-                done = true;
-                break;
-            }
-            x += 1;
-            c += 1;
-        }
-        if done {
-            break;
-        }
-
-        // down
-        for _ in 0..l {
-            table[y][x] = c;
-            if y >= n - 1 {
-                done = true;
-                break;
-            }
-            y += 1;
-            c += 1;
-        }
-        if done {
-            break;
-        }
-
-        l += 1;
-
-        // left
-        for _ in 0..l {
-            table[y][x] = c;
-            if x == 0 {
-                done = true;
-                break;
-            }
-            x -= 1;
-            c += 1;
-        }
-        if done {
-            break;
-        }
-
-        // up
-        for _ in 0..l {
-            table[y][x] = c;
-            if y == 0 {
-                done = true;
-                break;
-            }
-            y -= 1;
-            c += 1;
-        }
-        if done {
-            break;
+fn problem_0028(n: usize) -> usize {
+    let mut result = 1;
+    let mut i = 1;
+    for step in (2..n).step_by(2) {
+        for _ in 0..4 {
+            i += step;
+            result += i;
         }
     }
-
-    let mut result = 0;
-    for i in 0..n {
-        result += table[i][i];
-        result += table[i][n - i - 1];
-    }
-    result -= table[n / 2][n / 2];
-
     result
 }
 
